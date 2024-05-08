dd81a5382b5796f7c03f002db19bf2dcb15dfe0f
Gaurav Shukla <gaurav.shukla@rxlogix.com>
2024-05-08 16:44:11 +0530

    updated ./sb001/index1.js

--- sb001/index1.js
+++ sb001/index1.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname,"public")));
+// The below app.use would also work given a public folder exists in cuurent directory
+// and there is the specified html file (index.html by default)
+// app.use(express.static("public"));
 app.listen(3000, () => {console.log('Lister active at port 3000.')});
