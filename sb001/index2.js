8aeca608f23c72b9c32806d0bb00c37aefbee1fd
Gaurav Shukla <gaurav.shukla@rxlogix.com>
2024-05-09 10:19:35 +0530

    added functionality for Dynamic content

--- sb001/index2.js
+++ sb001/index2.js
@@ -6,7 +6,8 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/src/templates/views");
 app.get("/", (req,res) => {
 res.render("index", {
-    author:"GauSh"
+    title:"HBS Dynamic Template",
+    msg:"This is dynamically fetched content."
 });
 });
 app.listen(3000, () => {console.log("Express Listener running at port 3000")});
--- sb001/index3.js
+++ sb001/index3.js
@@ -4,9 +4,19 @@ const hbs = require("hbs");
 const app = express();
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/src/templates/views");
+hbs.registerPartials(path.join(__dirname, "/src/templates/partials"));
 app.get("/", (req,res) => {
 res.render("index", {
-    author:"GauSh"
+    title:"HBS Dynamic Template",
+    author:"GauSh",
+    msg:"This is dynamically fetched content."
+});
+});
+app.get("/about", (req,res) => {
+    res.render("index", {
+        title:"HBS Dynamic Template",
+        author:"GauSh",
+        msg:"This is a dynamic abouts page."
 });
 });
 app.listen(3000, () => {console.log("Express Listener running at port 3000")});
